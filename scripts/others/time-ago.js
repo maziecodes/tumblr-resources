e37e9f97f24664d740f0f9431ec068928384be12
maziekeen's codes <69180924+maziecodes@users.noreply.github.com>
2025-11-02 17:28:32 -0300

    Update time-ago.js

--- scripts/others/time-ago.js
+++ scripts/others/time-ago.js
@@ -1,39 +1,32 @@
-(function() {
-    'use strict';
+const formatNoteCount = (() => {
+    const formatter = new Intl.NumberFormat('en-US', {
+        notation: 'compact',
+        compactDisplay: 'short',
+        maximumFractionDigits: 1
+    });
     
-    const formatNoteCount = (() => {
-        const formatter = new Intl.NumberFormat('en-US', {
-            notation: 'compact',
-            compactDisplay: 'short',
-            maximumFractionDigits: 1
-        });
-        return (num) => formatter.format(num);
-    })();
+    return (num) => formatter.format(num);
+});
 
-    const initNoteCountFormatting = (options = {}) => {
-        const {
-            containerSelector = '.theme-container',
-            itemSelector = '.notecount',
-            threshold = 999
-        } = options;
+const initNoteCountFormatting = (options = {}) => {
+    const {
+        containerSelector = '.theme-container',
+        itemSelector = '.notecount',
+        threshold = 999,
+        locale = 'en-US'
+    } = options;
+    
+    const container = document.querySelector(containerSelector);
+    if (!container) return;
+    
+    const noteCounts = container.querySelectorAll(itemSelector);
+    
+    for (let i = 0; i < noteCounts.length; i++) {
+        const element = noteCounts[i];
+        const number = parseInt(element.textContent.replace(/[,\s]/g, ''), 10);
         
-        const container = document.querySelector(containerSelector);
-        if (!container) return;
-        
-        const noteCounts = container.querySelectorAll(itemSelector);
-        
-        for (let i = 0; i < noteCounts.length; i++) {
-            const element = noteCounts[i];
-            const number = parseInt(element.textContent.replace(/[,\s]/g, ''), 10);
-            
-            if (number > threshold) {
-                element.textContent = formatNoteCount(number);
-            }
+        if (number > threshold) {
+            element.textContent = formatNoteCount(number);
         }
-    };
-
-    window.TumblrNoteFormatter = {
-        init: initNoteCountFormatting
-    };
-    
-})();
+    }
+};
